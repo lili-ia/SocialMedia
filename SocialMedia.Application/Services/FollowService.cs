161bbc425c6f8e9cbcef8ee23b21b6935603bc92
liliia <andycraygo@gmail.com>
2025-07-15 11:27:13 +0200

    refactor(follow): add detailed logging, catch DbUpdateException on duplicate follow request, remove automapper and use manual mapping instead, use FirstOrDefault and AsNoTracking to optimize reading

--- SocialMedia.Application/Services/FollowService.cs
+++ SocialMedia.Application/Services/FollowService.cs
@@ -1,6 +1,6 @@
-﻿using AutoMapper;
-using Domain.Entities;
+﻿using Domain.Entities;
 using Domain.Events;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using SocialMedia.Application.Contracts;
@@ -13,35 +13,47 @@ public class FollowService : IFollowService
 {
     private readonly SocialMediaContext _db;
     private readonly ILogger<FollowService> _logger;
-    private readonly IMapper _mapper;
     private readonly IEventProducer _eventProducer;
 
     public FollowService(
         SocialMediaContext db, 
         ILogger<FollowService> logger, 
-        IMapper mapper,
         IEventProducer eventProducer)
     {
         _db = db;
         _logger = logger;
-        _mapper = mapper;
         _eventProducer = eventProducer;
     }
 
     public async Task<Result<FollowDto>> FollowAsync(Guid followerId, Guid followeeId, CancellationToken ct)
     {
-        var follower = await _db.Users.FindAsync(followerId);
+        _logger.LogInformation("User with id {FollowerId} attempts to follow user with id {FolloweeId}.", followerId, followeeId);
+
+        if (followerId == followeeId)
+        {
+            _logger.LogWarning("User with ID {FollowerId} attempts to follow themselves.", followerId);
+            
+            return Result<FollowDto>.FailureResult("You can not follow yourself.", ErrorType.Forbidden);
+        }
+        
+        var follower = await _db.Users.AsNoTracking()
+            .FirstOrDefaultAsync(u => u.Id == followerId, ct);
 
         if (follower == null)
         {
-            return Result<FollowDto>.FailureResult("Follower was not found.");
+            _logger.LogWarning("User with ID {FollowerId} not found.", followerId);
+            
+            return Result<FollowDto>.FailureResult("Follower not found.");
         }
         
-        var followee = await _db.Users.FindAsync(followeeId);
+        var followee = await _db.Users.AsNoTracking()
+            .FirstOrDefaultAsync(u => u.Id == followeeId, ct);
 
         if (followee == null)
         {
-            return Result<FollowDto>.FailureResult("Followee was not found.", ErrorType.NotFound);
+            _logger.LogWarning("User with ID {FolloweeId} not found.", followeeId);
+            
+            return Result<FollowDto>.FailureResult("Followee not found.", ErrorType.NotFound);
         }
 
         var followExists = await _db.Follows
@@ -49,21 +61,25 @@ public class FollowService : IFollowService
 
         if (followExists)
         {
-            return Result<FollowDto>.FailureResult("You're already following this user", ErrorType.Forbidden);
+            _logger.LogWarning("User with ID {FollowerId} already follows user with id {FolloweeId}.", followerId, followeeId);
+            
+            return Result<FollowDto>.FailureResult("You are already following this user", ErrorType.Forbidden);
         }
-        
+
+        var followTime = DateTime.UtcNow;
         var newFollow = new Follow
         {
             FolloweeId = followeeId,
             FollowerId = followerId,
-            FollowedAt = DateTime.UtcNow
+            FollowedAt = followTime
         };
 
         try
         {
             await _db.Follows.AddAsync(newFollow, ct);
             await _db.SaveChangesAsync(ct);
-
+            _logger.LogInformation("User with id {FollowerId} now follows user with id {FolloweeId}.", followerId, followeeId);
+            
             var evt = new FollowedEvent
             {
                 FollowerId = followerId,
@@ -74,17 +90,29 @@ public class FollowService : IFollowService
             };
 
             await _eventProducer.SendMessageAsync("follows-topic", evt, ct);
+            _logger.LogInformation("Follow notification was successfully sent to followee with id {FolloweeId}.", followeeId);
+        }
+        catch (DbUpdateException e) when (e.InnerException is SqlException { Number: 2601 or 2627 })
+        {
+            _logger.LogWarning("Duplicate follow request detected: {FollowerId} → {FolloweeId}", followerId, followeeId);
             
-            var followDto = _mapper.Map<FollowDto>(newFollow);
-            
-            return Result<FollowDto>.SuccessResult(followDto);
+            return Result<FollowDto>.FailureResult("You're already following this user", ErrorType.Forbidden);
         }
         catch (Exception ex)
         {
             _logger.LogError(ex, "An error occurred while {FollowerId} trying to follow {FolloweeId}", followerId, followeeId);
             
-            return Result<FollowDto>.FailureResult("Internal server error.", ErrorType.ServerError);
+            return Result<FollowDto>.FailureResult("An internal error occured.", ErrorType.ServerError);
         }
+
+        var followDto = new FollowDto
+        {
+            FolloweeId = followeeId,
+            FollowerId = followerId,
+            FollowedAt = followTime
+        };
+                     
+        return Result<FollowDto>.SuccessResult(followDto);
     }
 
     public Task<Result<bool>> UnfollowUserAsync(Guid followerId, Guid followeeId, CancellationToken ct)
