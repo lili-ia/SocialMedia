fe79f6c472577b60c4d7b281be3d0162c281827d
liliia <andycraygo@gmail.com>
2025-09-01 17:14:49 +0200

    refactor: remove unused classes

--- src/SocialMedia.Application/Contracts/ISendMessageUseCase.cs
+++
@@ -1,8 +0,0 @@
-﻿using Domain.Entities;
-
-namespace SocialMedia.Application.Contracts;
-
-public interface ISendMessageUseCase
-{
-    Task<Message> ExecuteAsync(Guid chatId, string content, Guid senderId, CancellationToken ct);
-}
--- src/SocialMedia.Infrastructure/Messaging/Models/KafkaEnvelope.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Infrastructure.Messaging.Models;
-
-public class KafkaEnvelope
-{
-    
-}
