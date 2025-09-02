af4c39345989e37d4c6c96c0699c78dfd4d8d119
liliia <andycraygo@gmail.com>
2025-09-02 10:18:47 +0200

    refactor(db): switch to postgres, apply init migration

---
+++ src/SocialMedia.Persistence/SocialMediaDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using SocialMedia.Persistence.Configurations;
+
+namespace SocialMedia.Persistence;
+
+public class SocialMediaDbContext : DbContext
+{
+    public SocialMediaDbContext()
+    {
+    }
+
+    public SocialMediaDbContext(DbContextOptions<SocialMediaDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Chat> Chats { get; set; }
+
+    public virtual DbSet<Comment> Comments { get; set; }
+
+    public virtual DbSet<Message> Messages { get; set; }
+
+    public virtual DbSet<Post> Posts { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+    
+    public virtual DbSet<RefreshToken> RefreshTokens { get; set; }
+    
+    public virtual DbSet<PasswordResetToken> PasswordResetTokens { get; set; }
+    
+    public virtual DbSet<EmailConfirmationToken> EmailConfirmationTokens { get; set; }
+    
+    public virtual DbSet<PostLike> PostLikes { get; set; }
+    
+    public virtual DbSet<PostView> PostViews { get; set; }
+    
+    public virtual DbSet<Follow> Follows { get; set; }
+    
+    public virtual DbSet<Notification> Notifications { get; set; }
+    
+    public virtual DbSet<Block> Blocks { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(UserConfiguration).Assembly);
+    }
+}
